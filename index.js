5b098d5e318ad8eb44e7faabc8ae6927feaa2aa5
James <jjguevarag@gmail.com>
2024-02-06 19:15:24 -0600

    .

--- index.js
+++ index.js
@@ -1,40 +1,59 @@
-import { ApolloServer, gql } from "apollo-server";
+import { ApolloServer, UserInputError, gql } from "apollo-server";
+import { v1 as uuid } from "uuid";
 
 const persons = [
   {
     name: "Rocky",
+    age: "11",
     phone: "034-1234567",
     street: "Calle Frontend",
-    city: "Barcelona",
+    city: "Guarari",
     id: "3d594650-3436-11e9-bc57-8b80ba54c431",
   },
   {
     name: "James",
+    age: "23",
     phone: "044-123456",
     street: "Avenida Fullstack",
-    city: "Mataro",
+    city: "Heredia",
     id: "3d599470-3436-11e9-bc57-8b80ba54c431",
   },
   {
     name: "Cookie",
+    age: "19",
     street: "Pasaje Testing",
-    city: "Ibiza",
+    city: "Los lagos",
     id: "3d599471-3436-11e9-bc57-8b80ba54c431",
   },
 ];
 
 const typeDefs = gql`
+  type Address {
+    street: String!
+    city: String!
+  }
+
   type Person {
     name: String!
     phone: String
-    street: String!
-    city: String!
+    address: Address!
+    canDrink: Boolean!
     id: ID!
   }
 
   type Query {
     personCount: Int!
     allPersons: [Person]!
+    findPerson(name: String!): Person
+  }
+
+  type Mutation {
+    addPerson(
+      name: String!
+      phone: String!
+      street: String!
+      city: String!
+    ): Person
   }
 `;
 
@@ -42,6 +61,32 @@ const resolvers = {
   Query: {
     personCount: () => persons.length,
     allPersons: () => persons,
+    findPerson: (root, args) => {
+      const { name } = args;
+      return persons.find((person) => person.name === name);
+    },
+  },
+  Mutation: {
+    addPerson: (root, args) => {
+      if (persons.find((p) => p.name === args.name)) {
+        throw new UserInputError("Name must be unique", {
+          invalidArgs: args.name,
+        });
+      }
+      // const { name, phone , street , city } = args
+      const person = { ...args, id: uuid() };
+      persons.push(person); //update database with new person
+      return person;
+    },
+  },
+  Person: {
+    canDrink: (root) => root.age > 18,
+    address: (root) => {
+      return {
+        street: root.street,
+        city: root.city,
+      };
+    },
   },
 };
 
